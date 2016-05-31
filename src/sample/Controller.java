fbc123c9e7bcbf9df5f1e4c99edd100663e4aef5
willriggins <williamriggins2013@gmail.com>
2016-05-31 13:53:05 -0400

    initial load

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    TextField name;
+
+    @FXML
+    TextField phone;
+
+    @FXML
+    TextField email;
+
+}
