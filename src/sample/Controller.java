baf7b0ed015b044ba2dead115fc2aa2095a25ff0
willriggins <williamriggins2013@gmail.com>
2016-05-31 22:06:27 -0400

    requirements complete

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -26,13 +27,17 @@ public class Controller implements Initializable {
     ObservableList<Contact> contacts = FXCollections.observableArrayList();
 
     public void onAdd() {
-        Contact contact = new  Contact(name.getText(), phone.getText(), email.getText());
-        contacts.add(contact);
-        clearAll();
+        Contact contact = new Contact(name.getText(), phone.getText(), email.getText());
+        if (!contact.name.equals("") && !contact.phone.equals("") && !contact.email.equals("")) {
+            contacts.add(contact);
+            clearAll();
+        }
     }
 
     public void onRemove() {
-
+        SelectionModel model = list.getSelectionModel();
+        Contact contact = (Contact) model.getSelectedItem();
+        contacts.remove(contact);
     }
 
     public void clearAll() {
